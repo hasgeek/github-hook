abb682e25ef1b0a7916740602209dfe47c136f17
Bibhas <me@bibhas.in>
2019-06-19 15:09:19 +0530

    checking for hmac signature and event type

--- github-hook.py
+++ github-hook.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os, os.path
+import hmac
+import hashlib
 import json
 import subprocess
-from flask import Flask, request, redirect, abort
+from flask import Flask, request, redirect, abort, Response
 app = Flask(__name__)
 
 PROJECTS_ROOT = '/var/www'
@@ -25,8 +27,15 @@ def commit():
     payload = request.form.get('payload')
     if not payload:
         return "Missing form variable 'payload'"
+
+    signature = 'sha1=' + hmac.new(WEBHOOK_SECRET, request.body, hashlib.sha1).hexdigest()
+    if 'X-Hub-Signature' not in request.headers or request.headers.get('X-Hub-Signature') != signature:
+        return "Signature doesn't match", 401
+
+    if not request.headers.get('X-GitHub-Event') == 'push':
+        return "Only push events are allowed", 401
+
     payload = json.loads(payload)
-    print payload
     reponame = payload['repository']['name']
     savedir = os.getcwd()
     repodir = os.path.join(PROJECTS_ROOT, reponame, reponame)  # will fail for hgtv
@@ -34,14 +43,14 @@ def commit():
     if reponame == 'funnel':
         os.chdir(repodir)
         current_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], env=os.environ).rstrip('\n')
-        if payload['ref'] == "refs/heads/{staging_branch}".format(staging_branch=current_branch):
+        if 'ref' in payload and payload['ref'] == "refs/heads/{staging_branch}".format(staging_branch=current_branch):
             if os.access(repodir, os.W_OK | os.X_OK):
                 pull_output = subprocess.check_output(['git', 'pull', 'origin', current_branch], env=os.environ)  # if failed, it'll raise exception
 
                 os.chdir(savedir)  # Is this really required?
                 return pull_output
     else:
-        return "Unknown repository, or no access"
+        return "Unknown repository, or no access", 401
 
 
 application = app  # For WSGI
